93b1669889fcea6cc9eb03f93393c2ad60001b04
Palloma Ruysa <pallomaruysa@gmail.com>
2021-09-05 16:12:49 -0300

    atividade com c# while

---
+++ Exercicio-09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numAprovados = 0, numReprovados = 0, contadorAlunos = 1, resultadoExame;
+
+            while (contadorAlunos <= 10)
+            {
+                 Console.WriteLine("Digite Resultado (1 = APROVAR |  2 = REPROVAR): ");
+                 resultadoExame = Int32.Parse(Console.ReadLine());
+
+                 if(resultadoExame == 1)
+                    numAprovados++;
+                 
+                 else
+                    numReprovados++;  
+
+                contadorAlunos++;    
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Aprovados: "+numAprovados);
+            Console.WriteLine("Reprovados: "+numReprovados);
+
+            if(numAprovados > 8 )
+                Console.WriteLine("Aumentar valor do curso\n");
+        }
+        
+    }
+}
---
+++ Exercicio-10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadorClientesFila = 1, temCliente = 0, semCliente = 0, resultadoClientesFila;
+
+            while (contadorClientesFila <= 15)
+            {
+                Console.WriteLine("Digite se tem cliente na fila (1 = Tem cliente na fila | 2 = Não tem cliente na fila");
+                resultadoClientesFila = Int32.Parse(Console.ReadLine());
+
+                if (resultadoClientesFila == 1)    
+                        temCliente++;
+
+                else
+                     semCliente++;
+
+                contadorClientesFila++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pessoas na fila: "+temCliente);
+            Console.WriteLine("Pessoas já atendidas: "+semCliente);
+            Console.WriteLine();
+
+            if(temCliente >= 15)
+                Console.WriteLine("Chame mais um funcionário para atender os clientes\n");
+        }
+    }
+}

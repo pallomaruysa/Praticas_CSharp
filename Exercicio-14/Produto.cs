53b2f122bc811f8baa87cb4981cc18b217c1b7f7
Palloma Ruysa <pallomaruysa@gmail.com>
2021-10-23 11:07:22 -0300

    incrementando ArrayList

--- Exercicio-14/Produto.cs
+++ Exercicio-14/Produto.cs
@@ -5,6 +5,7 @@ namespace Exercicio_14
 {
     public class Produto
     {
+        public int Codigo;
         public string  Nome;
         public double  Preco;
         public int  Quantidade;
@@ -12,9 +13,16 @@ namespace Exercicio_14
         public double ValorTotalEmEstoque(){
             return Preco * Quantidade;
         }
+        public void AdicionarProduto(int quantidade){
+                Quantidade +=quantidade;
+        }
+
+        public void RemoverProduto(int quantidade){
+                Quantidade -=quantidade;
+        }
 
         public override string ToString(){
-            return Nome+", R$ "+
+            return "\nId do produto: "+Codigo+"  |  "+Nome+", R$ "+
             Preco.ToString("F2", CultureInfo.InvariantCulture)+" com "+
             Quantidade+" no estoque. \nTotal: R$ "+
             ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
--- Exercicio-14/Program.cs
+++ Exercicio-14/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System;
+using System.Collections;
 
 namespace Exercicio_14
 {
@@ -7,18 +8,91 @@ namespace Exercicio_14
     {
         static void Main(string[] args)
         {
-            Produto p = new Produto();
-            Console.WriteLine("Digite os dados do produto");
-            Console.WriteLine("Nome: ");
-            p.Nome = Console.ReadLine();
-            Console.WriteLine("Preço: ");
-            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Quantidade no estoque: ");
-            p.Quantidade = int.Parse(Console.ReadLine());
+            ArrayList listaProduto = new ArrayList();
+            int escolha;
+            
+            do{
+                Console.WriteLine("\nMENU: ");
+                Console.WriteLine("1: Cadastrar Novo Produto");
+                Console.WriteLine("2: Alterar Produto");                
+                Console.WriteLine("3: Remover Produto");
+                Console.WriteLine("4: Vizualizar Lista de Produtos");
+                Console.WriteLine("5: Sair \n");
 
+                escolha = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Dados do Produto: "+p);
+                Produto p = new Produto();
+                
+                switch(escolha){
+                    case 1:                    
+                            Console.WriteLine("\nDigite os dados do produto\n");
+                            Console.WriteLine("Código: ");
+                            p.Codigo = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Nome: ");
+                            p.Nome = Console.ReadLine();
+                            Console.WriteLine("Preço: ");
+                            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                            Console.WriteLine("Quantidade no estoque: ");
+                            p.Quantidade = int.Parse(Console.ReadLine());
+                            Console.WriteLine("------------------------------------------------------");
+                            listaProduto.Add(p);
+                    break;
+                    
+                    case 2: 
+                            Console.WriteLine("\n1: Adicionar quantidade do produto no estoque");
+                            Console.WriteLine("2: Remover quantidade do produto no estoque");
 
+                            int escolhaAcao = int.Parse(Console.ReadLine());
+
+                            switch(escolhaAcao){
+                                case 1: 
+                                        Console.WriteLine("Digite o ID do produto a ser modificado: ");
+                                        int id1 = int.Parse(Console.ReadLine());
+                                        Console.WriteLine("Digite a quantidade de produtos a ser adicionado: ");
+                                        p.AdicionarProduto(int.Parse(Console.ReadLine()));
+                                break;
+                                case 2:
+                                        Console.WriteLine("Digite o ID do produto a ser modificado: ");
+                                        int id2 = int.Parse(Console.ReadLine());
+                                        Console.WriteLine("Digite a quantidade de produtos a ser removido: ");
+                                        p.RemoverProduto(int.Parse(Console.ReadLine()));
+                                break;
+                            }
+                    break;
+
+                    case 3:
+                            Console.WriteLine("\nDigite o código do produto a ser removido");
+                            int id = int.Parse(Console.ReadLine());
+                            
+                            for(int i=0; i< listaProduto.Count; i++){
+                                if(id == p.Codigo){
+                                    listaProduto.RemoveAt(id);    
+                                    Console.WriteLine("Código do produto removido com sucesso \n");      
+                                }
+                                else{
+                                    Console.WriteLine("Código do produto inexistente");
+                                }
+                            }
+                        break;
+                            
+                    case 4:
+                            foreach(Produto i in listaProduto){
+                                    Console.WriteLine("\nDados do Produto: "+i);
+                            }                            
+                    break;
+
+                    case 5: 
+                             Console.WriteLine("Saindo da Operação");
+                    break;
+
+                    default:
+                            Console.WriteLine("Opção inválida! Digite um valor que esteja no menu");
+                    break;
+                            
+                }
+
+            } while(escolha != 5);
         }
+            
     }
 }

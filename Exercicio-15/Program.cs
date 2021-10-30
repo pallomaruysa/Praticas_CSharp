9f905167bcee3fec91180269d64572b225cf3ee9
Palloma Ruysa <pallomaruysa@gmail.com>
2021-10-29 22:55:23 -0300

    ajustando codigos

--- Exercicio-15/Program.cs
+++ Exercicio-15/Program.cs
@@ -13,10 +13,10 @@ namespace Exercicio_15
             double contNotas=0.0;
 
             Console.WriteLine("\n \t\t\t\t Seja bem-vindo ao WellPlus");
-            Console.WriteLine("\t\t Digite aqui suas notas para saber como vai seu rendimento ;-)\n");
+            Console.WriteLine("\t\t Digite aqui suas notas para saber como vai seu rendimento ;-)");
 
             do{
-                Console.WriteLine("Digite o nome da matéria");
+                Console.WriteLine("\nDigite o nome da matéria");
                 n.Materia = Console.ReadLine();
 
                 Console.WriteLine("Digite quantas notas deseja adicionar");
@@ -26,24 +26,27 @@ namespace Exercicio_15
                     Console.WriteLine("\nNota: ");
                     n.Nota = double.Parse(Console.ReadLine());
                     listaNotas.Add(n.Nota);
-                }
-                
-
-                Console.WriteLine("RESULTADO:");
-                
-                for(int i=0; i<listaNotas.Count; i++){
-                    Console.WriteLine(listaNotas[i]);
                     contNotas+=(double)listaNotas[i];
                 }
 
+
+
+                Console.WriteLine("\nRESULTADO: ");
                 
-                Console.WriteLine("\nMédia: "+n.Media(contNotas, listaNotas.Count));
+                for(int i=0; i<qntNotas; i++){
+                    Console.WriteLine(listaNotas[i]);
+                }
+
+                Console.WriteLine("\nMédia: "+n.Media(contNotas, qntNotas));
 
 
                 Console.WriteLine("-----------------------------------------------------------------------------\n\n");
-             
+
+                listaNotas.Clear();
+
                 Console.WriteLine("Deseja adicionar mais alguma nota? (1 - sim | 2 - não)");
                 loop = int.Parse(Console.ReadLine());
+                Console.WriteLine("-----------------------------------------------------------------------------\n\n");
             }while(loop != 2);
         }
     }

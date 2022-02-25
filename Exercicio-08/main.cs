375af305c7d23ac1ace7e641343ff50718e26876
pallomaruysa <pallomaruysa@gmail.com>
2022-02-25 09:23:53 -0300

    exemplo de Console.WriteLine

---
+++ Exercicio-08/main.cs
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+ int c;   
+c = 5;
+            Console.WriteLine(c);
+            Console.WriteLine(c++);
+            Console.WriteLine(c);
+
+c = 5;
+            Console.WriteLine(c);
+            Console.WriteLine(++c);
+            Console.WriteLine(c);
+  }
+}
